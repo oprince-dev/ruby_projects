2c38de8d74a43a963a7f8c4effde196363f1ab71
Oliver Prince <oprince14@gmail.com>
2016-10-07 23:06:41 -0500

    Save data to json

--- bank_accounts.rb
+++ bank_accounts.rb
@@ -1,3 +1,5 @@
+require 'json'
+
 class BankAccount
   attr_reader :name
   attr_reader :timestamp
@@ -6,6 +8,17 @@ class BankAccount
     @name = name
     @timestamp = timestamp
     @transactions = []
+  end
+
+  def save
+    File.open("#{name}_account.json", "w+") do |f|
+      f.write(JSON.pretty_generate(@transactions))
+    end
+  end
+
+  def open
+    f = File.read("#{name}_account.json")
+    @transactions = JSON.parse(f,:symbolize_names => true)
   end
 
   def add_withdraw(description, amount, timestamp)
@@ -57,14 +70,39 @@ def ask(q, kind="string")
     return answer
 end
 
+def user_welcome
+  answer = ask("Welcome.\n\nWould you like to:\n\t(1) Create an account\n\t(2) Login\n")
+  if answer == "1"
+    user_add_account
+  elsif answer == "2"
+    user_login
+  else
+    user_welcome
+  end
+end
+
 def user_add_account
   account_name = ask("Account Name:")
   timestamp = user_add_timestamp
+
   bank_account = BankAccount.new(account_name, timestamp)
-  answer = ""
   initial_deposit = ask("How much would you like to add for your initial deposit?:")
+
   bank_account.add_transaction("Initial Deposit", initial_deposit.to_i, timestamp)
+  bank_account.save
   bank_account.print_register
+
+  user_deposit_withdraw(bank_account)
+end
+
+def user_login
+  account_name = ask("Account Name: ")
+  timestamp = user_add_timestamp
+
+  bank_account = BankAccount.new(account_name, timestamp)
+  bank_account.open
+  bank_account.print_register
+
   user_deposit_withdraw(bank_account)
 end
 
@@ -72,26 +110,30 @@ def user_deposit_withdraw(bank_account)
   while bank_account != ""
     answer = ask("Would you like to deposit or withdraw?:").downcase
     if answer == "deposit"
-      timestamp = user_add_timestamp.to_s
-      user_add_deposit(bank_account, timestamp)
+      user_add_deposit(bank_account)
     elsif answer == "withdraw"
-      timestamp = user_add_timestamp.to_s
-      user_add_withdraw(bank_account, timestamp)
+      user_add_withdraw(bank_account)
     end
   end
 end
 
-def user_add_deposit(bank_account, timestamp)
+def user_add_deposit(bank_account)
   deposit_description = ask("Add a description to this deposit:")
   deposit_amount = ask("How much would you like to deposit?:").to_i
+  timestamp = user_add_timestamp.to_s
+
   bank_account.add_deposit(deposit_description, deposit_amount, timestamp)
+  bank_account.save
   bank_account.print_register
 end
 
-def user_add_withdraw(bank_account, timestamp)
+def user_add_withdraw(bank_account)
   withdraw_description = ask("Add a description to this withdraw:")
   withdraw_amount = ask("How much would you like to withdraw?:").to_i
+  timestamp = user_add_timestamp.to_s
+
   bank_account.add_withdraw(withdraw_description, withdraw_amount, timestamp)
+  bank_account.save
   bank_account.print_register
 end
 
@@ -100,5 +142,6 @@ def user_add_timestamp
   return timestamp
 end
 
+
 user_add_timestamp
-user_add_account
+user_welcome
